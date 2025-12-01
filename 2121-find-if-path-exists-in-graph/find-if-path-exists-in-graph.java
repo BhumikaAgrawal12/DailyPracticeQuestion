ac215e1580d8e62c40978e6e16f13f74ac6a829b
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2025-12-02 00:31:37 +0530

    Time: 183 ms (27.02%) | Memory: 283.1 MB (20.87%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,6 +1,7 @@
+//dfs 
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        ArrayList<List<Integer>>adj=new ArrayList<>();
+         ArrayList<List<Integer>>adj=new ArrayList<>();
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
@@ -11,24 +12,19 @@ class Solution {
             adj.get(v).add(u);
         }
         int[] visited=new int[n];
-        return BFS(adj,visited,source,destination);
+        return dfs(adj,visited,source,destination);
     }
-    public static boolean BFS(ArrayList<List<Integer>>adj,int[] visited,int node,int dest){
-        Queue<Integer>q=new LinkedList<>();
-        q.offer(node);
+    boolean dfs(ArrayList<List<Integer>>adj,int[] visited,int node,int dest){
+        if(node==dest){
+            return true;
+        }
         visited[node]=1;
-        while(!q.isEmpty()){
-            int front_node=q.poll();
-            if(front_node==dest){
-                return true;
-            }
-            for(int nbr:adj.get(front_node)){
-                if(visited[nbr]==0){
-                    q.offer(nbr);
-                    visited[nbr]=1;
+        for(int nbr:adj.get(node)){
+            if(visited[nbr]==0){
+                if(dfs(adj,visited,nbr,dest)){
+                    return true;
                 }
             }
-            
         }
         return false;
     }
