5d715f6d587f18d4d23fa5a6c2dae0ba23dde9f7
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2026-01-06 11:30:50 +0530

    Time: 17 ms (55.88%) | Memory: 55.2 MB (34.26%) - LeetSync

---
+++ 922-possible-bipartition/possible-bipartition.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean possibleBipartition(int n, int[][] dislikes) {
+        List<List<Integer>>adj=new ArrayList<>();
+        for(int i=0;i<=n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge:dislikes){
+            int u=edge[0];
+            int v=edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        Queue<Integer>q=new LinkedList<>();
+        int[] color=new int[n+1];
+        Arrays.fill(color,-1);
+        for(int i=1;i<=n;i++){
+            if(color[i]==-1){
+                color[i]=0;
+                q.add(i);
+            }
+            while(!q.isEmpty()){
+                int node=q.poll();
+                for(int nbr:adj.get(node)){
+                    if(color[nbr]==-1){
+                        color[nbr]=1-color[node];
+                        q.add(nbr);
+                    }
+                    else if(color[node]==color[nbr]){
+                        return false;
+                    }
+                }
+            }
+
+        }
+        return true;
+
+    }
+}
