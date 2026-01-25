65f858fb416566e5af4fcf233af60604ae8f38d1
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2026-01-25 14:20:18 +0530

    Time: 15 ms (61.87%) | Memory: 51.7 MB (28.83%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -2,9 +2,9 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        int cnt=0;
         int i=0;
         int j=0;
+        int cnt=0;
         while(i<g.length && j<s.length){
             if(s[j]>=g[i]){
                 cnt++;
@@ -14,8 +14,6 @@ class Solution {
             else{
                 j++;
             }
-
-            
         }
         return cnt;
     }
