553d394c8a8fb33252cf796cba5efb20545b78a5
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2026-01-30 14:50:17 +0530

    Time: 52 ms (8.07%) | Memory: 101.2 MB (6.34%) - LeetSync

--- 1814-jump-game-vi/jump-game-vi.java
+++ 1814-jump-game-vi/jump-game-vi.java
@@ -3,15 +3,15 @@ class Solution {
         int n=nums.length;
         int[] dp=new int[n];
         PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->b[0]-a[0]);
-        pq.add(new int[]{nums[n-1],n-1});
-        dp[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            while(!pq.isEmpty() && pq.peek()[1]>i+k){
+        pq.add(new int[]{nums[0],0});
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            while(!pq.isEmpty() && pq.peek()[1]<i-k){
                 pq.poll();
             }
             dp[i]=nums[i]+pq.peek()[0];
             pq.add(new int[]{dp[i],i});
         }
-        return dp[0];
+        return dp[n-1];
     }
 }
