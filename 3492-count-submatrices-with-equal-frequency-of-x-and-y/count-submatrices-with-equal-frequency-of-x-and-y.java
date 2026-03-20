ead6b8abc0ca65e11e66174852566cc67a6a1a12
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2026-03-20 11:41:18 +0530

    Time: 17 ms (98.11%) | Memory: 260.1 MB (7.55%) - LeetSync

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.java
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -3,29 +3,20 @@ class Solution {
         int m = grid.length, n = grid[0].length;
         int ans = 0;
 
-        int[][] px = new int[m][n];
-        int[][] py = new int[m][n];
+        int[] px = new int[n];
+        int[] py = new int[n];
 
         for (int i = 0; i < m; i++) {
+            int rowX = 0, rowY = 0;
+
             for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 'X') rowX++;
+                else if (grid[i][j] == 'Y') rowY++;
 
-                px[i][j] = (grid[i][j] == 'X') ? 1 : 0;
-                py[i][j] = (grid[i][j] == 'Y') ? 1 : 0;
+                px[j] += rowX;
+                py[j] += rowY;
 
-                if (i > 0) {
-                    px[i][j] += px[i - 1][j];
-                    py[i][j] += py[i - 1][j];
-                }
-                if (j > 0) {
-                    px[i][j] += px[i][j - 1];
-                    py[i][j] += py[i][j - 1];
-                }
-                if (i > 0 && j > 0) {
-                    px[i][j] -= px[i - 1][j - 1];
-                    py[i][j] -= py[i - 1][j - 1];
-                }
-
-                if (px[i][j] == py[i][j] && px[i][j] > 0) {
+                if (px[j] == py[j] && px[j] > 0) {
                     ans++;
                 }
             }
