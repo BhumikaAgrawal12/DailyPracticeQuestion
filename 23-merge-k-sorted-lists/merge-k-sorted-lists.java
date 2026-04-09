1d17a8c07d10c207689570620ca9b3f86635069c
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2026-04-09 11:35:08 +0530

    Time: 5 ms (41.87%) | Memory: 46.8 MB (69.59%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -38,40 +38,66 @@
 // }
 
 //approach->Merge 2sorted lists
+// class Solution {
+//     public ListNode mergeKLists(ListNode[] lists) {
+//         if(lists==null || lists.length==0){
+//             return null;
+//         }
+//         ListNode head=lists[0];
+//         for(int i=1;i<lists.length;i++){
+//             head=merge(head,lists[i]);
+//         }
+//         return head;
+//     }
+//     public ListNode merge (ListNode one,ListNode two){
+//         ListNode dummy=new ListNode(-1);
+//         ListNode tail=dummy;
+//         ListNode curr1=one;
+//         ListNode curr2=two;
+//         while(curr1!=null && curr2!=null){
+//             if(curr1.val<curr2.val){
+//                 tail.next=curr1;
+//                 curr1=curr1.next;
+//             }
+//             else{
+//                 tail.next=curr2;
+//                 curr2=curr2.next;
+//             }
+//             tail=tail.next;
+//         }
+//         if(curr1!=null){
+//             tail.next=curr1;
+//             curr1=curr1.next;
+//         }
+//         if(curr2!=null){
+//             tail.next=curr2;
+//             curr2=curr2.next;
+//         }
+//         return dummy.next;
+//     }
+// }
+
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists==null || lists.length==0){
-            return null;
-        }
-        ListNode head=lists[0];
-        for(int i=1;i<lists.length;i++){
-            head=merge(head,lists[i]);
-        }
-        return head;
-    }
-    public ListNode merge (ListNode one,ListNode two){
+        if(lists == null || lists.length == 0) return null;
+
+        PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b)->a.val-b.val);
+
         ListNode dummy=new ListNode(-1);
-        ListNode tail=dummy;
-        ListNode curr1=one;
-        ListNode curr2=two;
-        while(curr1!=null && curr2!=null){
-            if(curr1.val<curr2.val){
-                tail.next=curr1;
-                curr1=curr1.next;
+        ListNode temp=dummy;
+
+        for(int i=0;i<lists.length;i++){
+             if(lists[i] != null){
+                pq.add(lists[i]);
             }
-            else{
-                tail.next=curr2;
-                curr2=curr2.next;
+        }
+        while(!pq.isEmpty()){
+            ListNode curr=pq.poll();
+            temp.next=curr;
+            if(curr.next != null){
+                pq.add(curr.next);
             }
-            tail=tail.next;
-        }
-        if(curr1!=null){
-            tail.next=curr1;
-            curr1=curr1.next;
-        }
-        if(curr2!=null){
-            tail.next=curr2;
-            curr2=curr2.next;
+            temp=temp.next;
         }
         return dummy.next;
     }
