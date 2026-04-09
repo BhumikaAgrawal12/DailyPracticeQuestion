e91f8027b7bbb700f188af3ac3c8da5825f0876d
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2026-04-09 11:13:46 +0530

    Time: 82 ms (12.78%) | Memory: 46.9 MB (35.75%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,29 +10,69 @@
  */
 
  //brute force
+// class Solution {
+//     public ListNode mergeKLists(ListNode[] lists) {
+//         List<Integer> arr=new ArrayList<>();
+//         int n=lists.length;
+//         for(int i=0;i<n;i++){
+//             ListNode temp=lists[i];
+//             while(temp!=null){
+//                 arr.add(temp.val);
+//                 temp=temp.next;
+//             }
+//         }
+//         if(arr.size() == 0) return null;
+//         Collections.sort(arr);
+//         ListNode head=convert(arr);
+//         return head;
+//     }
+//     public ListNode convert(List<Integer> arr){
+//         ListNode head=new ListNode(arr.get(0));
+//         ListNode curr=head;
+//         for(int i=1;i<arr.size();i++){
+//             curr.next=new ListNode(arr.get(i));
+//             curr=curr.next;
+//         }
+//         return head;
+//     }
+// }
+
+//approach->Merge 2sorted lists
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        List<Integer> arr=new ArrayList<>();
-        int n=lists.length;
-        for(int i=0;i<n;i++){
-            ListNode temp=lists[i];
-            while(temp!=null){
-                arr.add(temp.val);
-                temp=temp.next;
-            }
+        if(lists==null || lists.length==0){
+            return null;
         }
-        if(arr.size() == 0) return null;
-        Collections.sort(arr);
-        ListNode head=convert(arr);
+        ListNode head=lists[0];
+        for(int i=1;i<lists.length;i++){
+            head=merge(head,lists[i]);
+        }
         return head;
     }
-    public ListNode convert(List<Integer> arr){
-        ListNode head=new ListNode(arr.get(0));
-        ListNode curr=head;
-        for(int i=1;i<arr.size();i++){
-            curr.next=new ListNode(arr.get(i));
-            curr=curr.next;
+    public ListNode merge (ListNode one,ListNode two){
+        ListNode dummy=new ListNode(-1);
+        ListNode tail=dummy;
+        ListNode curr1=one;
+        ListNode curr2=two;
+        while(curr1!=null && curr2!=null){
+            if(curr1.val<curr2.val){
+                tail.next=curr1;
+                curr1=curr1.next;
+            }
+            else{
+                tail.next=curr2;
+                curr2=curr2.next;
+            }
+            tail=tail.next;
         }
-        return head;
+        if(curr1!=null){
+            tail.next=curr1;
+            curr1=curr1.next;
+        }
+        if(curr2!=null){
+            tail.next=curr2;
+            curr2=curr2.next;
+        }
+        return dummy.next;
     }
 }
