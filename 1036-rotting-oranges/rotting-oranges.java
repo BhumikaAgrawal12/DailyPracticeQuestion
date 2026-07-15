0ef4b89ad0d31d5c8ad8010df89b9f20e0a69f21
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2026-07-15 11:53:59 +0530

    Time: 2 ms (91.20%) | Memory: 44.2 MB (40.54%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,43 +2,40 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        Queue<int[]>q=new LinkedList<>();
-        int[][] visited=new int[m][n];
-        int cntFresh=0;
+        int fresh=0;
+        Queue<int[]> q=new LinkedList<>();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
-                    visited[i][j]=2;
                     q.offer(new int[]{i,j,0});
                 }
-                if(grid[i][j]==1){
-                    cntFresh++;
+                else if(grid[i][j]==1){
+                    fresh++;
                 }
             }
         }
+        if(fresh==0){
+            return 0;
+        }
+        int[][] direction={{-1,0},{1,0},{0,-1},{0,1}};
         int time=0;
-        int[] dr={-1,0,1,0};
-        int[] dc={0,-1,0,1};
-        int cnt=0;
         while(!q.isEmpty()){
-            int[] arr=q.poll();
-            int r=arr[0];
-            int c=arr[1];
-            int t=arr[2];
-            time=Math.max(t,time);
-            for(int i=0;i<4;i++){
-                int row=r+dr[i];
-                int col=c+dc[i];
-                if(row>=0 && col>=0 && row<m && col<n && grid[row][col]==1 && visited[row][col]==0){
-                    q.add(new int[]{row,col,t+1});
-                    visited[row][col]=2;
-                    cnt++;
+            int[] curr=q.poll();
+            int r=curr[0];
+            int c=curr[1];
+            int t=curr[2];
+            time=Math.max(time,t);
+            for(int[] dir:direction){
+                int rd=r+dir[0];
+                int cd=c+dir[1];
+                if(rd>=0 && rd<m && cd>=0 && cd<n && grid[rd][cd]==1){
+                    grid[rd][cd]=2;
+                    fresh--;
+                    q.offer(new int[]{rd,cd,t+1});
                 }
             }
+
         }
-        if(cntFresh!=cnt){
-            return -1;
-        }
-        return time;
+        return fresh==0?time:-1;
     }
 }
