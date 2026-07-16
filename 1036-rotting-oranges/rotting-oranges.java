b75d79184af9c5e271856bdccb1057df53e1280b
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2026-07-16 11:39:56 +0530

    Time: 2 ms (91.25%) | Memory: 44.4 MB (13.97%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -17,8 +17,8 @@ class Solution {
         if(fresh==0){
             return 0;
         }
-        int[][] direction={{-1,0},{1,0},{0,-1},{0,1}};
         int time=0;
+        int[][] direction={{-1,0},{1,0},{0,1},{0,-1}};
         while(!q.isEmpty()){
             int[] curr=q.poll();
             int r=curr[0];
@@ -26,15 +26,14 @@ class Solution {
             int t=curr[2];
             time=Math.max(time,t);
             for(int[] dir:direction){
-                int rd=r+dir[0];
-                int cd=c+dir[1];
-                if(rd>=0 && rd<m && cd>=0 && cd<n && grid[rd][cd]==1){
-                    grid[rd][cd]=2;
+                int rc=dir[0]+r;
+                int cr=dir[1]+c;
+                if(rc>=0 && rc<m && cr>=0 && cr<n && grid[rc][cr]==1){
+                    grid[rc][cr]=2;
                     fresh--;
-                    q.offer(new int[]{rd,cd,t+1});
+                    q.offer(new int[]{rc,cr,t+1});
                 }
             }
-
         }
         return fresh==0?time:-1;
     }
