49a23b047ca5f81f53075fd1b7030840234825d6
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2026-07-20 13:28:35 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (14.93%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -15,30 +15,41 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-       //  List<List<Integer>> ans=new ArrayList<>();
         if(root==null){
             return 0;
         }
-        
-        Queue<TreeNode> q=new LinkedList<>();
-        q.add(root);
-        int height=0;
-        while(!q.isEmpty()){
-            List<Integer> level=new ArrayList<>();
-            int len=q.size();
-            height++;
-            for(int i=0;i<len;i++){
-                TreeNode node=q.poll();
-               // level.add(node.val);
-                if(node.left!=null){
-                    q.add(node.left);
-                }
-                if(node.right!=null){
-                    q.add(node.right);
-                }
-            }
-            //ans.add(level);
-        }
-        return height;
+        int leftSubtree=maxDepth(root.left);
+        int rightSubtree=maxDepth(root.right);
+        return 1+Math.max(leftSubtree,rightSubtree);
     }
 }
+
+// class Solution {
+//     public int maxDepth(TreeNode root) {
+//        //  List<List<Integer>> ans=new ArrayList<>();
+//         if(root==null){
+//             return 0;
+//         }
+        
+//         Queue<TreeNode> q=new LinkedList<>();
+//         q.add(root);
+//         int height=0;
+//         while(!q.isEmpty()){
+//             List<Integer> level=new ArrayList<>();
+//             int len=q.size();
+//             height++;
+//             for(int i=0;i<len;i++){
+//                 TreeNode node=q.poll();
+//                // level.add(node.val);
+//                 if(node.left!=null){
+//                     q.add(node.left);
+//                 }
+//                 if(node.right!=null){
+//                     q.add(node.right);
+//                 }
+//             }
+//             //ans.add(level);
+//         }
+//         return height;
+//     }
+// }
