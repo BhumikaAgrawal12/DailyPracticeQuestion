c824a0212cbc243307fcf261769d9b87310dd04b
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2026-07-20 13:44:36 +0530

    Time: 1 ms (31.82%) | Memory: 45.1 MB (98.82%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -13,16 +13,14 @@
  *     }
  * }
  */
-
- //Top Down approach(0(n^2))
 class Solution {
     public boolean isBalanced(TreeNode root) {
         if(root==null){
             return true;
         }
-        int lh=height(root.left);
-        int rh=height(root.right);
-        if(Math.abs(lh-rh)>1){
+        int left=height(root.left);
+        int right=height(root.right);
+        if(Math.abs(left-right)>1){
             return false;
         }
         return isBalanced(root.left) && isBalanced(root.right);
@@ -31,39 +29,8 @@ class Solution {
         if(root==null){
             return 0;
         }
-        return Math.max(height(root.left),height(root.right))+1;
+        int leftSubtree=height(root.left);
+        int rightSubtree=height(root.right);
+        return 1+Math.max(leftSubtree,rightSubtree);
     }
 }
-
-//Bottom Up approach(O(n))
-// class Solution {
-//     public boolean isBalanced(TreeNode root) {
-//         if(root==null){
-//             return true;
-//         }
-//         return checkBalanceAndHeight(root) != -1;
-//     }
-
-//     // Returns height if balanced, -1 if unbalanced
-//     private int checkBalanceAndHeight(TreeNode node) {
-//         if (node == null) {
-//             return 0; // Height of an empty tree is 0
-//         }
-
-//         int leftHeight = checkBalanceAndHeight(node.left);
-//         if (leftHeight == -1) {
-//             return -1; // Left subtree is unbalanced
-//         }
-
-//         int rightHeight = checkBalanceAndHeight(node.right);
-//         if (rightHeight == -1) {
-//             return -1; // Right subtree is unbalanced
-//         }
-
-//         if (Math.abs(leftHeight - rightHeight) > 1) {
-//             return -1; // Current node is unbalanced
-//         }
-
-//         return 1 + Math.max(leftHeight, rightHeight); // Return height of current node
-//     }
-// }
