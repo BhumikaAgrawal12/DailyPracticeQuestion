140bbe0e79139061857208ecf6a839cf60fa547b
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2026-07-20 10:45:07 +0530

    Time: 1 ms (98.01%) | Memory: 46.9 MB (89.04%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
+        int n=intervals.length;
         List<int[]> res=new ArrayList<>();
         for(int[] slot:intervals){
             if(newInterval[1]<slot[0]){
                 res.add(newInterval);
                 newInterval=slot;
             }
-            else if(slot[1]<newInterval[0]){
+            else if(newInterval[0]>slot[1]){
                 res.add(slot);
             }
             else{
-                newInterval[0]=Math.min(slot[0],newInterval[0]);
-                newInterval[1]=Math.max(slot[1],newInterval[1]);
+                newInterval[0]=Math.min(newInterval[0],slot[0]);
+                newInterval[1]=Math.max(newInterval[1],slot[1]);
             }
         }
         res.add(newInterval);
