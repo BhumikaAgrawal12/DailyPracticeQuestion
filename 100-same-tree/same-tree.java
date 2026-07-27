70ea2d8589316f5b82e69f179093fb3f95dfc821
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2026-07-27 10:27:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (33.99%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -24,6 +24,5 @@ class Solution {
         boolean left=isSameTree(p.left,q.left);
         boolean right=isSameTree(p.right,q.right);
         return p.val==q.val && left==true && right==true;
-
     }
 }
