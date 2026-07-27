70c4e4f2142d987ce846c1a854b0be79554fbe06
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2026-07-27 11:00:30 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (10.05%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -18,30 +18,20 @@ class Solution {
         if(root==null){
             return true;
         }
-        // int left=height(root.left);
-        // int right=height(root.right);
-        // if(Math.abs(left-right)>1){
-        //     return false;
-        // }
-        // return isBalanced(root.left) && isBalanced(root.right);
-        return height(root)!=-1;
+        int lh=height(root.left);
+        int rh=height(root.right);
+        if(Math.abs(lh-rh)>1){
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+        
     }
-    public int height(TreeNode root){
+    int height(TreeNode root){
         if(root==null){
             return 0;
         }
-        int lh=height(root.left);
-        if(lh==-1){
-            return -1;
-        }
-        int rh=height(root.right);
-        if(rh==-1){
-            return -1;
-        }
-
-        if(Math.abs(lh-rh)>1){
-            return -1;
-        }
-        return 1+Math.max(lh,rh);
+        int left=height(root.left);
+        int right=height(root.right);
+        return 1+Math.max(left,right);
     }
 }
