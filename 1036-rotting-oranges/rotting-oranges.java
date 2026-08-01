0febc05ccb429ae67e805458f5ad1bfbda8b6729
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2026-08-01 22:41:07 +0530

    Time: 2 ms (89.84%) | Memory: 43.9 MB (85.42%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -3,14 +3,14 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         int fresh=0;
-        Queue<int[]> q=new LinkedList<>();
+        Queue<int[]> q=new LinkedList<>(); 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    fresh++;
+                }
                 if(grid[i][j]==2){
                     q.offer(new int[]{i,j,0});
-                }
-                else if(grid[i][j]==1){
-                    fresh++;
                 }
             }
         }
@@ -18,20 +18,20 @@ class Solution {
             return 0;
         }
         int time=0;
-        int[][] direction={{-1,0},{1,0},{0,1},{0,-1}};
+        int[][] dir={{-1,0},{0,-1},{0,1},{1,0}};
         while(!q.isEmpty()){
             int[] curr=q.poll();
-            int r=curr[0];
-            int c=curr[1];
+            int row=curr[0];
+            int col=curr[1];
             int t=curr[2];
             time=Math.max(time,t);
-            for(int[] dir:direction){
-                int rc=dir[0]+r;
-                int cr=dir[1]+c;
-                if(rc>=0 && rc<m && cr>=0 && cr<n && grid[rc][cr]==1){
-                    grid[rc][cr]=2;
+            for(int[] d:dir){
+                int cr=d[0]+row;
+                int cc=d[1]+col;
+                if(cr>=0 && cr<m && cc>=0 && cc<n && grid[cr][cc]==1){
+                    grid[cr][cc]=2;
                     fresh--;
-                    q.offer(new int[]{rc,cr,t+1});
+                    q.add(new int[]{cr,cc,t+1});
                 }
             }
         }
