71530218cc52d13cbd9ab821aa10591a979af12e
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2026-08-11 09:55:52 +0530

    Time: 7 ms (56.13%) | Memory: 47.4 MB (21.46%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,33 +1,29 @@
 class Solution {
     public boolean canFinish(int V, int[][] edges) {
-       List<List<Integer>>adj=new ArrayList<>();
+        List<Integer> ans=new ArrayList<>();
+        List<List<Integer>> adj=new ArrayList<>();
         for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
         }
-        for(int[] edge:edges){
+        int[] inD=new int[V];
+        for(int[]edge:edges){
             int u=edge[0];
             int v=edge[1];
+            inD[v]++;
             adj.get(u).add(v);
         }
-        int[] indegree=new int[V];
+        Queue<Integer> q=new LinkedList<>();
         for(int i=0;i<V;i++){
-            for(int nbr:adj.get(i)){
-                indegree[nbr]++;
-            }
-        }
-        Queue<Integer>q=new LinkedList<>();
-        for(int i=0;i<V;i++){
-            if(indegree[i]==0){
+            if(inD[i]==0){
                 q.offer(i);
             }
         }
-        ArrayList<Integer>ans=new ArrayList<>();
         while(!q.isEmpty()){
             int node=q.poll();
             ans.add(node);
             for(int nbr:adj.get(node)){
-                indegree[nbr]--;
-                if(indegree[nbr]==0){
+                inD[nbr]--;
+                if(inD[nbr]==0){
                     q.offer(nbr);
                 }
             }
