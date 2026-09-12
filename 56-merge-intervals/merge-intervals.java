2a047baaf34b4019ab8c56ab3be9558c4a3c4b1f
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2026-09-12 14:04:11 +0530

    Time: 8 ms (90.11%) | Memory: 49.2 MB (26.63%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,9 +2,9 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
         int n=intervals.length;
+        int indx=0;
         int[][] res=new int[n][2];
         int[] prev=intervals[0];
-        int indx=0;
         for(int[] curr:intervals){
             if(prev[1]>=curr[0]){
                 prev[1]=Math.max(prev[1],curr[1]);
@@ -14,7 +14,7 @@ class Solution {
                 prev=curr;
             }
         }
-       res[indx++]=prev;
+        res[indx++]=prev;
         return Arrays.copyOf(res,indx);
     }
 }
